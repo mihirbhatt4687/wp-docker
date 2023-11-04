907e46d7d62ae15d1c46a1f527db94c1aa6aa5c6
Mihir <mihir@localpc.com>
2023-11-04 19:04:25 +0530

    Initial Commit

---
+++ doc-root/index.php
@@ -0,0 +1,20 @@
+<?php
+
+phpinfo();
+// ini_set('display_errors', 1);
+// #echo WORDPRESS_DB_HOST;
+
+// $servername = "mysql";
+// $username = "myuser";
+// $password = "mypassword";
+// $database = "wordpress";
+
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
